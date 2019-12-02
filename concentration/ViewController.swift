3bbec3f85cb1f2caa967d09df10a6b9e6796e3ae
shane <smmiller4337@eagle.fgcu.edu>
2019-12-01 20:56:53 -0500

    Initial Commit

---
+++ concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by shane mill on 12/1/19.
+//  Copyright © 2019 shane mill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
